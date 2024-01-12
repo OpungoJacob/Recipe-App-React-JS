ab2837e0abff2ec8739278ce7690b45495cf2408
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2024-01-12 15:12:34 +0300

    new
    edits

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -2,16 +2,17 @@ import { useEffect } from "react";
 // import dotenv from "dotenv";
 // dotenv.config();
 
+
 function Popular() {
 
-    // const API_KEY='178b89a5516a49859ebe5a39bf1afbe2';
+  const API_KEY='178b89a5516a49859ebe5a39bf1afbe2';
 
     useEffect(() => {
         getPopular();
     },[]); 
 
     const getPopular = async () => {
-        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.API_KEY}&number=9`);
+        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${API_KEY}&number=10`);
         const data = await api.json();
         console.log(data);
     }
