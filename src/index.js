d0f558ef23962155e80040c76d0c1af3fc5e7b7e
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2024-01-12 13:18:12 +0300

    new
    new

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import App from './App'
 
-const root = ReactDOM.render
+// ReactDOM.render(
+//     <App />
+// ,
+// document.getElementById('root'))
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
