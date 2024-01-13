dd998e6f875052c685c3c91915c33d8ca8fb0b90
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2024-01-13 16:45:08 +0300

    Update Popular.jsx
    new

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -9,17 +9,18 @@ function Popular() {
 
   // const API_KEY='178b89a5516a49859ebe5a39bf1afbe2';
   const API_KEY = process.env.REACT_APP_API_KEY;
-  console.log("API_KEY:", API_KEY); 
+  // console.log("API_KEY:", API_KEY); 
 
     useEffect(() => {
         getPopular();
-    },[]); 
+    }, []); 
 
     const getPopular = async () => {
         const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${API_KEY}&number=10`);
         const data = await api.json();
-        console.log(data);
+        // console.log(data);
         // console.log("API_KEY:", API_KEY);
+        setPopular(data.recipes);
         
     }
 
