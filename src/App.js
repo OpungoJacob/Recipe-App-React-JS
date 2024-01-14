6d824fdcb456851beb6adf2328be518fcc5c6c2d
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2024-01-14 14:08:21 +0300

    category
    new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Pages from './pages/Pages'
+import Category from './components/Category'
 
 function App() {
   return (
     <div>
-      
+      <Category/>
       <Pages />
     </div>
   )
