157f46f1a47d6e8b5aa2e6b9c7690e138eb3df0b
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2024-01-15 19:08:35 +0300

    edits
    edits

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import Pages from './pages/Pages'
+import React from 'react';
+import Pages from './pages/Pages';
 import Category from './components/Category'
 import {BrowserRouter} from 'react-router-dom'
 function App() {
