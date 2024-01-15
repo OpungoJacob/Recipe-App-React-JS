505af298637f5d3bab66d2da53d40861adb19eec
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2024-01-15 19:08:52 +0300

    Update App.js
    new

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Pages from './pages/Pages';
-import Category from './components/Category'
-import {BrowserRouter} from 'react-router-dom'
+import Category from './components/Category';
+import {BrowserRouter} from 'react-router-dom';
 function App() {
   return (
     <div>
