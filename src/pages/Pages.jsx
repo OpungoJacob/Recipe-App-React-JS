7fa9286bdc4ef3f3ce2343a630392a5087c0c7e0
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2024-01-15 12:41:51 +0300

    Update Pages.jsx
    new

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import Home from './Home'
-import {Route, Routes} from 'react-router-dom'
+import React from 'react';
+import Home from './Home';
+import {Route, Routes} from 'react-router-dom';
 
 function Pages() {
   return (
