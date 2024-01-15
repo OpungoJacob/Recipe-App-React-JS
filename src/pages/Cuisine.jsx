d1f5a98bd2e0e269e7b2ab13c01bf20b4a8fb7fa
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2024-01-15 13:12:18 +0300

    Update Cuisine.jsx
    new

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import styled from 'styled-components'
-import {motion} from 'framer-motion'
-import { Link, useParams  } from 'react-router-dom'
+import React from 'react';
+import styled from 'styled-components';
+import {motion} from 'framer-motion';
+import { Link, useParams  } from 'react-router-dom';
 
 
 function Cuisine() {
+
+    const API_KEY = process.env.REACT_APP_API_KEY;
+
+
+    const getCuisine = async(name) => {
+        const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&number=10&cuisine=${name}`);
+    }
+
   return (
-    <div>Cuisine</div>
+    <div>
+
+    </div>
   )
 }
 
