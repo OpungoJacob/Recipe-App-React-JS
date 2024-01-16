95a7bbf68e12d994e9aa67666d508dfc96c5028d
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2024-01-16 14:09:16 +0300

    Update Search.jsx
    new

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -37,11 +37,12 @@ const FormStyle = styled.form`
         background: linear-gradient(35deg, #494949, #313131);
         font-size: 1.5rem;
         color: white;
-        padding: 1rem 3 rem;
+        padding: 1rem 3rem;
         border: none;
         border-radius: 1rem;
         outline: none;
         width: 100%;
+        height: 3rem;
     }
     svg{
         position: absolute;
