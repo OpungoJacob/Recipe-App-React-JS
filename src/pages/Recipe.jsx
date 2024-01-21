dc05d560a9a54308f2e650b64071cad48754f39b
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2024-01-21 19:48:08 +0300

    Update Recipe.jsx
    new

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -3,6 +3,15 @@ import styled from "styled-components";
 import { useParams } from "react-router-dom";
 
 function Recipe() {
+
+    const API_KEY = process.env.REACT_APP_API_KEY;
+
+    let params = useParams();
+
+    const fetchDetails = async () => {
+        const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${API_KEY}`);
+    }
+
   return (
     <div>Recipe</div>
   )
