606bbb632e6704fc8d793a7767cf65882043e77f
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2024-01-21 19:58:17 +0300

    Update Recipe.jsx
    new

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -7,9 +7,12 @@ function Recipe() {
     const API_KEY = process.env.REACT_APP_API_KEY;
 
     let params = useParams();
+    const [details, setDetails] = useState({});
 
     const fetchDetails = async () => {
         const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${API_KEY}`);
+        const detailData = await data.json();
+        setDetails(detailData)
     }
 
   return (
