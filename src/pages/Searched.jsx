632effe3baa1d535664bc8b365993f6690ac0cbb
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2024-01-21 17:18:24 +0300

    Update Searched.jsx
    new

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 function Searched() {
 
   const API_KEY = process.env.REACT_APP_API_KEY;
 
   const [serachedRecipes, setSearchedRecipes] = useState([]);
+  let params = useParams();
 
   const getSearched = async(name) => {
     const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&query=${name}`);
     const recipes = await data.json();
-    setCuisine(recipes.results);
+    setSearchedRecipes(recipes.results);
 };
+
+  useEffect(() => {
+    getSearched(params.search);
+  },[params.search]);
 
   return (
     <div>Searched</div>
