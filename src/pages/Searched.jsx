098c7ec3d9fcb76e85d60473817b1ea80d8e5a1f
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2024-01-21 17:20:12 +0300

    Update Searched.jsx
    new

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
 
 function Searched() {
 
@@ -23,5 +24,27 @@ function Searched() {
     <div>Searched</div>
   )
 }
+
+const Grid = styled.div`
+display: grid;
+grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+grid-gap: 3rem;
+`;
+
+const Card = styled.div`
+ img{
+    width: 100%;
+    border-radius: 2rem;
+ }
+
+ a {
+    text-decoration: none;
+ }
+
+ h4{
+    text-align: center;
+    padding: 1rem ;
+ }
+`;
 
 export default Searched
