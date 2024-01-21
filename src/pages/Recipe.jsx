33eae0f1f34aba0a6c0aa4f706e11390e48e1896
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2024-01-21 20:26:26 +0300

    Update Recipe.jsx
    new

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -28,12 +28,23 @@ const DetailWrapper = styled.div`
     margin-top: 10rem;
     margin-bottom: 5rem;
     display: flex;
+    .active{
+        background: linear-gradient(35deg, #494949, #313131) ;
+        color: white;
+    }
     h2{
         margin-bottom: 2rem;
     }
     li{
-        font-size
+        font-size: 1.2rem;
+        line-height: 2.5rem;
+    }
+    ul{
+        margin-top: 2rem;
+
     }
 `
+
+
 
 export default Recipe
