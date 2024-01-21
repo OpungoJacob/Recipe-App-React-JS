164c0fcefd1323363b17127c108b6ee646cc7067
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2024-01-21 17:40:52 +0300

    new

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -3,6 +3,7 @@ import Home from './Home';
 import Cuisine from './Cuisine';
 import {Route, Routes} from 'react-router-dom';
 import Searched from './Searched';
+import Recipe from './Recipe';
 
 
 function Pages() {
@@ -12,7 +13,7 @@ function Pages() {
         <Route path="/" element={ <Home />}/>
         <Route path ="/cuisine/:cuisine" element={<Cuisine />}/>
         <Route path="/searched/:search" element={<Searched />}/>
-        
+        <Route path="recipe/:name" element={<Recipe />}/>
     </Routes>
    
   )
