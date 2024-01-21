34f2d1b578b73547531ed6f9d035d0c144d27872
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2024-01-21 20:28:01 +0300

    Update Recipe.jsx
    new

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -45,6 +45,11 @@ const DetailWrapper = styled.div`
     }
 `
 
-
+ const Button = styled.button`
+  padding: 1rem 2rem;
+  color: #313131;
+  background: white;
+  border: 2px solid black'  
+ `
 
 export default Recipe
