2d69f6b2caa5d2c5f411b939ab5700506ad8adda
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2024-01-22 10:14:38 +0300

    Update App.js
    new

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import Category from './components/Category';
 import {BrowserRouter} from 'react-router-dom';
 import Search from './components/Search';
 import styled from 'styled-components';
+import { GiKnifeFork } from 'react-icons/gi';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+      <Nav>
+        <GiKnifeFork />
+        <Logo to={"/"} > deliciousss</Logo>
+      </Nav>
       <Search />
       <Category/>
       <Pages />
@@ -26,7 +31,12 @@ const Logo = styled(Link)`
 const Nav = styled.div`
   padding: 4rem 0rem;
   display: flex;
-  
+  justify-content: flex-start;
+  align-items: center; 
+
+  svg{
+    font-size: 2rem;
+  }
 
 `;
 
