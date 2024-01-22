9548af2cb853dc3b11731ffdc8d484370d75904f
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2024-01-22 10:09:29 +0300

    Update App.js
    new

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Pages from './pages/Pages';
 import Category from './components/Category';
 import {BrowserRouter} from 'react-router-dom';
 import Search from './components/Search';
-
+import styled from 'styled-components';
 
 function App() {
   return (
@@ -16,5 +16,18 @@ function App() {
     </div>
   )
 }
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 400;
+  font-family: 'Lobster Two', cursive;
+`;
+
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  
+
+`;
 
 export default App
